359c1267cdcec8d023adf5113e7d21ef8f6dd17f
muskanfatima5 <160497988+muskanfatima5@users.noreply.github.com>
2024-11-24 23:06:27 +0500

    Update nav.tsx

--- app/components/navigation/nav.tsx
+++ app/components/navigation/nav.tsx
@@ -1,8 +1,8 @@
 function Navbar(){
     return(
     <div className="bg-yellow-950 text-white flex justify-evenly items-center
-     md:flex-col sm:items-center sm:w-14 sm:h-80
-     lg:flex-row lg:w-full lg:h-10">
+     md:flex-row sm:items-center sm:w-14 sm:h-80
+     lg:flex-col lg:w-full lg:h-10">
        <a href="/">About</a>
        <a href="/">Home</a>
        <a href="/">Contact</a>
