a5eb15fc4842a893405c10d97628f535988c915f
muskanfatima5 <160497988+muskanfatima5@users.noreply.github.com>
2024-11-24 23:45:59 +0500

    Add files via upload

---
+++ app/components/navigation/nav.tsx
@@ -0,0 +1,10 @@
+function Navbar(){
+    return(
+    <div className="bg-yellow-950 w-14 h-40 text-white flex justify-evenly items-center flex-col 
+     md:flex-row md:items-center md:w-full md:h-10 
+     lg:w-full lg:h-10 lg:items-center lg:flex-row lg:flex">
+       <a href="/">About</a>
+       <a href="/">Home</a>
+       <a href="/">Contact</a>
+    </div>)
+}export default Navbar
