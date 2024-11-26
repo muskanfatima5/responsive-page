c9f6cfa6310b5cce0698d64d7f686c3ffaa40019
muskanfatima5 <160497988+muskanfatima5@users.noreply.github.com>
2024-11-26 22:46:54 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,12 @@ function Home(){
   return(
     <div className="flex md:flex-col lg:flex-col">
     <Navbar />
-  <div className="bg-gray-800 flex justify-center text-white md:flex-col lg:flex-row">
-    <div className="bg-slate-500 h-[160px] lg:h-48 lg:w-[33.3%]">
+  <div className="bg-gray-800 flex justify-center font-bold text-white md:flex-col lg:flex-row">
+    <div className="bg-slate-500 h-screen lg:h-48 lg:w-[33.3%]">
       Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
-      <div className="bg-black h-40 lg:h-48 md:w-[50%] lg:w-[33.3%]">
+      <div className="bg-black h-screen lg:h-48 md:w-[50%] lg:w-[33.3%]">
       Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
-      <div className="bg-blue-600 h-40 lg:h-48 md:w-[50%] lg:w-[33.3%]">
+      <div className="bg-blue-600 h-screen lg:h-48 md:w-[50%] lg:w-[33.3%]">
       Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
       </div>
       </div>)
